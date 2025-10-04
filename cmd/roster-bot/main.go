78fc6e33def2d08ee4d00bf1c3b0fc573510529b
Korzhavin Ivan <korjavin@gmail.com>
2025-10-04 20:20:41 +0200

    fix .gitignore

---
+++ cmd/roster-bot/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	log.Println("Roster Bot starting...")
+	fmt.Println("Roster Bot v0.1.0 - Duty Schedule Management System")
+	log.Println("Application initialized successfully")
+}
